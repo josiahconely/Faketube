7bca7bfcd28f6560d9a3d93762b894bebf06ab81
irfancheemaa <38930333+irfancheemaa@users.noreply.github.com>
2020-02-09 16:14:42 -0600

    getters and setters
    created getters and setters

---
+++ Fake_Tube/Fake_Tube/Classes/users.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fake_Tube.Classes
+{
+    public class users
+    {
+        private string userId; 
+        private string userName;
+        private string password;
+        private string coverImg;
+        private List<int> channelsSubcribed;
+        private List<int> channelsOwned;
+      
+
+        public string UserId
+        {
+            get => userId; 
+            set => userId = value; 
+        }
+        public string UserName
+        {
+            get => userName;
+            set => userName = value;
+
+        }
+        public string Password
+        {
+            get => password; 
+            set => password = value; 
+        }
+        public List<int> Subcribed
+        {
+            get => channelsSubcribed;
+            set => channelsSubcribed = value;
+        }
+
+        public List<int> Owned
+        {
+            get => channelsOwned;
+            set => channelsOwned = value;
+        }
+    }
+   
+}
